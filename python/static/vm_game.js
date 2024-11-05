7cfb4cd41620a844c88bfcaa230c691d2e8f9592
LLoydowski <janekbaszczynski@gmail.com>
2024-11-05 19:38:26 +0100

    made part of verbal backend

---
+++ python/static/vm_game.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("form")
+
+let choose = undefined 
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://127.0.0.1:85/verbal", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        "choose": choose
+    }));
+})
+
+function setChoose(value){
+    choose = value
+}
